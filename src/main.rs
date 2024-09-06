5d7a98d0d58c0bdd015a9bde005d12426bb574cc
thyrc <thyrc@users.noreply.github.com>
2024-09-06 18:40:41 +0200

    Replace clap with lexopt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use clap::{
-    builder::{RangedU64ValueParser, ValueParser},
-    Arg, ArgAction, ArgMatches, Command,
-};
 use log::{debug, error, info, warn};
 use simplelog::{
     ColorChoice, CombinedLogger, ConfigBuilder, LevelFilter, SharedLogger, TermLogger,
@@ -16,12 +12,49 @@ use std::{
     fs::{File, OpenOptions},
     io::{self, BufRead, BufReader},
     path::PathBuf,
-    process::{ExitStatus, Output},
-    sync::mpsc::{channel, Receiver, Sender},
-    sync::{Arc, Mutex},
+    process::{self, ExitStatus, Output},
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc, Mutex,
+    },
     thread,
 };
 use time::{Duration, Instant};
+
+const HELP: &str = "\
+Execute commands in parallel
+
+Usage: parallel-sh [OPTIONS] [clijobs]...
+
+Arguments:
+  [clijobs]...
+
+Options:
+  -q, --quiet           Do not print `parallel-sh` warnings
+  -n, --dry-run         Perform a trial run, only print what would be done (with -vv)
+  -v, --verbose...      Sets the level of verbosity
+  -l, --log <FILE>      Log output to file
+      --halt-on-error   Stop execution if an error occurs in any thread
+  -j, --jobs <THREADS>  Number of parallel executions
+  -s, --shell <SHELL>   Shell to use for command execution. Must support '-c' (defaults to sh)
+      --no-shell        Do not pass commands through a shell, but execute them directly
+  -f, --file <FILE>     Read commands from file (one command per line)
+  -h, --help            Print help
+  -V, --version         Print version
+";
+
+#[derive(Debug)]
+struct Args {
+    quiet: bool,
+    dryrun: bool,
+    verbose: usize,
+    logfile: Option<OsString>,
+    halt: bool,
+    threads: usize,
+    shell: Option<OsString>,
+    file: Option<OsString>,
+    clijobs: Vec<String>,
+}
 
 #[derive(Debug)]
 struct JobResult {
@@ -48,8 +81,92 @@ fn shared_channel<T>() -> (Sender<T>, SharedReceiver<T>) {
     (sender, SharedReceiver(Arc::new(Mutex::new(receiver))))
 }
 
-fn create_logger(opts: &ArgMatches) -> Result<(), std::io::Error> {
-    let level = match (opts.get_flag("quiet"), opts.get_count("verbose")) {
+fn parse_args() -> Result<Args, lexopt::Error> {
+    use lexopt::prelude::*;
+
+    let mut shell = if cfg!(target_os = "windows") {
+        Some(OsString::from("powershell"))
+    } else {
+        Some(OsString::from("sh"))
+    };
+
+    let mut quiet = false;
+    let mut dryrun = false;
+    let mut verbose = 0;
+    let mut logfile = None;
+    let mut halt = false;
+    let mut threads = num_cpus::get();
+    let mut file = None;
+    let mut clijobs = vec![];
+
+    let mut parser = lexopt::Parser::from_env();
+
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Short('q') | Long("quiet") => {
+                quiet = true;
+            }
+            Short('n') | Long("dry-run") => {
+                dryrun = true;
+            }
+            Short('v') | Long("verbose") => {
+                verbose += 1;
+            }
+            Short('l') | Long("log") => {
+                logfile = Some(parser.value()?.parse()?);
+            }
+            Long("halt-on-error") => {
+                halt = true;
+            }
+            Short('j') | Long("jobs") => {
+                threads = parser.value()?.parse()?;
+            }
+            Short('s') | Long("shell") => {
+                shell = Some(parser.value()?.parse()?);
+            }
+            Long("no-shell") => {
+                shell = None;
+            }
+            Short('f') | Long("file") => {
+                file = Some(parser.value()?.parse()?);
+            }
+            Short('h') | Long("help") => {
+                println!("{HELP}");
+                process::exit(0);
+            }
+            Short('V') | Long("version") => {
+                println!("{} {}", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION"));
+                process::exit(0);
+            }
+            //Value(_) if clijobs.is_empty() => {
+            //    let jobs: Vec<OsString> = parser.values()?.map(Into::into).collect();
+            //    clijobs = jobs
+            //        .iter()
+            //        .map(|os_str| os_str.to_string_lossy().to_string())
+            //        .collect();
+            //}
+            Value(value) => {
+                clijobs.push(value.string()?);
+            }
+            _ => return Err(arg.unexpected()),
+        }
+    }
+
+    Ok(Args {
+        quiet,
+        dryrun,
+        verbose,
+        logfile,
+        halt,
+        threads,
+        shell,
+        file,
+        clijobs,
+    })
+}
+
+fn create_logger(opts: &Args) -> Result<(), std::io::Error> {
+    let level = match (opts.quiet, opts.verbose) {
         (true, _) => LevelFilter::Error,
         (_, 0) => LevelFilter::Warn,
         (_, 1) => LevelFilter::Info,
@@ -70,7 +187,7 @@ fn create_logger(opts: &ArgMatches) -> Result<(), std::io::Error> {
         ColorChoice::Auto,
     )];
 
-    if let Some(file) = opts.get_one::<OsString>("log").map(PathBuf::from) {
+    if let Some(file) = opts.logfile.clone().map(PathBuf::from) {
         loggers.push(WriteLogger::new(
             level,
             logconfig,
@@ -99,11 +216,17 @@ fn add_jobs(
     if clijobs.is_empty() {
         if let Some(jobsfile) = jobsfile {
             let file = File::open(jobsfile)?;
-            BufReader::new(file).lines().flatten().for_each(start_job);
+            BufReader::new(file)
+                .lines()
+                .map_while(Result::ok)
+                .for_each(start_job);
         } else {
             let stdin = io::stdin();
             let handle = stdin.lock();
-            BufReader::new(handle).lines().flatten().for_each(start_job);
+            BufReader::new(handle)
+                .lines()
+                .map_while(Result::ok)
+                .for_each(start_job);
         }
     } else {
         // preferred
@@ -114,16 +237,16 @@ fn add_jobs(
 }
 
 fn run(dry_run: bool, command: &str, shell: &Option<OsString>) -> Output {
+    if dry_run {
+        return Output {
+            status: ExitStatus::default(),
+            stdout: Vec::new(),
+            stderr: Vec::new(),
+        };
+    };
+
     if let Some(s) = shell {
         let mut shell = std::process::Command::new(s);
-
-        if dry_run {
-            return Output {
-                status: ExitStatus::default(),
-                stdout: Vec::new(),
-                stderr: Vec::new(),
-            };
-        };
 
         match shell.arg("-c").arg(command).output() {
             Ok(s) => s,
@@ -136,14 +259,6 @@ fn run(dry_run: bool, command: &str, shell: &Option<OsString>) -> Output {
     } else {
         let cmd: Vec<_> = command.split(' ').collect();
         let mut command = std::process::Command::new(cmd[0]);
-
-        if dry_run {
-            return Output {
-                status: ExitStatus::default(),
-                stdout: Vec::new(),
-                stderr: Vec::new(),
-            };
-        };
 
         match command.args(&cmd[1..]).output() {
             Ok(c) => c,
@@ -189,101 +304,18 @@ fn start_workers(
     }
 }
 
-#[allow(clippy::too_many_lines)]
 fn main() {
-    let shell = if cfg!(target_os = "windows") {
-        OsString::from("powershell")
-    } else {
-        OsString::from("sh")
+    let args = match parse_args() {
+        Ok(args) => args,
+        Err(e) => {
+            eprintln!("ERROR: {e}");
+            process::exit(1);
+        }
     };
 
-    let matches = Command::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .override_usage(format!("{} [OPTIONS] [clijobs]...", env!("CARGO_BIN_NAME")))
-        .arg(
-            Arg::new("quiet")
-                .short('q')
-                .long("quiet")
-                .conflicts_with("verbose")
-                .action(ArgAction::SetTrue)
-                .help(format!(
-                    "Do not print `{}` warnings",
-                    env!("CARGO_BIN_NAME")
-                )),
-        )
-        .arg(
-            Arg::new("dry_run")
-                .short('n')
-                .long("dry-run")
-                .action(ArgAction::SetTrue)
-                .help("Perform a trial run, only print what would be done (with -vv)"),
-        )
-        .arg(
-            Arg::new("verbose")
-                .long("verbose")
-                .short('v')
-                .action(ArgAction::Count)
-                .conflicts_with("quiet")
-                .help("Sets the level of verbosity"),
-        )
-        .arg(
-            Arg::new("log")
-                .long("log")
-                .short('l')
-                .value_name("FILE")
-                .value_parser(ValueParser::os_string())
-                .num_args(1)
-                .help("Log output to file"),
-        )
-        .arg(
-            Arg::new("halt")
-                .long("halt-on-error")
-                .action(ArgAction::SetTrue)
-                .help("Stop execution if an error occurs in any thread"),
-        )
-        .arg(
-            Arg::new("threads")
-                .long("jobs")
-                .short('j')
-                .value_name("THREADS")
-                .value_parser(RangedU64ValueParser::<usize>::new().range(1..=4096))
-                .num_args(1)
-                .help("Number of parallel executions"),
-        )
-        .arg(
-            Arg::new("shell")
-                .long("shell")
-                .short('s')
-                .value_name("SHELL")
-                .value_parser(ValueParser::os_string())
-                .num_args(1)
-                .help(format!(
-                    "Shell to use for command execution. Must support '-c' (defaults to {})",
-                    shell.to_string_lossy()
-                )),
-        )
-        .arg(
-            Arg::new("noshell")
-                .long("no-shell")
-                .action(ArgAction::SetTrue)
-                .help("Do not pass commands through a shell, but execute them directly "),
-        )
-        .arg(
-            Arg::new("jobsfile")
-                .long("file")
-                .short('f')
-                .value_name("FILE")
-                .value_parser(ValueParser::os_string())
-                .num_args(1)
-                .help("Read commands from file (one command per line)"),
-        )
-        .arg(Arg::new("clijobs").action(ArgAction::Append))
-        .get_matches();
-
-    if let Err(e) = create_logger(&matches) {
+    if let Err(e) = create_logger(&args) {
         error!("Could create logger: {}", e);
-        std::process::exit(1);
+        process::exit(1);
     }
 
     let (tx, rx) = shared_channel();
@@ -291,44 +323,26 @@ fn main() {
     // return channel
     let (rtx, rrx) = channel();
 
-    let mut shell = Some(shell);
-    if let Some(v) = matches.get_one::<OsString>("shell") {
-        debug!("Using shell: '{}'", v.to_string_lossy());
-        shell = Some(v.clone());
+    let shell = if let Some(s) = args.shell {
+        debug!("Using shell: '{}'", s.to_string_lossy());
+        Some(s)
+    } else {
+        debug!("Running command without shell");
+        None
     };
 
-    if matches.get_flag("noshell") {
-        debug!("Running command without shell");
-        shell = None;
-    }
+    start_workers(args.threads, args.dryrun, &rx, rtx, &shell);
 
-    start_workers(
-        *matches.get_one("threads").unwrap_or(&num_cpus::get()),
-        matches.get_flag("dry_run"),
-        &rx,
-        rtx,
-        &shell,
-    );
+    let jobsfile = args.file.map(PathBuf::from);
 
-    let mut clijobs = vec![];
-    if matches.contains_id("clijobs") {
-        clijobs = matches
-            .get_many::<String>("clijobs")
-            .unwrap_or_default()
-            .cloned()
-            .collect::<Vec<_>>();
-    }
-
-    let jobsfile = matches.get_one::<OsString>("jobsfile").map(PathBuf::from);
-
-    if let Err(e) = add_jobs(clijobs, jobsfile, tx) {
+    if let Err(e) = add_jobs(args.clijobs, jobsfile, tx) {
         error!("Could not start jobs: {}", e);
         std::process::exit(1);
     }
 
     let mut exit = 0;
     for result in rrx {
-        if !matches.get_flag("dry_run") {
+        if !args.dryrun {
             info!(
                 "'{}' took {}.{}s",
                 &result.job,
@@ -343,7 +357,7 @@ fn main() {
                 print!("{}", String::from_utf8_lossy(&result.output.stdout));
                 eprint!("{}", String::from_utf8_lossy(&result.output.stderr));
 
-                if matches.get_flag("halt") {
+                if args.halt {
                     std::process::exit(1);
                 } else {
                     exit = result.output.status.code().unwrap_or(127);
